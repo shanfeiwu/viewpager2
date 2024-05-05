42c70e3532b4ec6469795f316a4a371cdbeb9a16
wushanfei <wsfnasn@163.com>
2024-05-05 23:04:51 +0800

    add CircularProgressBar

--- app/src/main/java/com/example/myapplication/ViewPager2FragmentFirst.kt
+++ app/src/main/java/com/example/myapplication/ViewPager2FragmentFirst.kt
@@ -5,14 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.myapplication.views.CircularProgressBar
 
 class ViewPager2FragmentFirst : Fragment() {
+    lateinit var circularProgressBar:CircularProgressBar
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.view_pager2_framgent_one,container,false)
+        var view =  inflater.inflate(R.layout.view_pager2_framgent_one,container,false)
+        circularProgressBar = view.findViewById(R.id.circluar_progress_bar)
+        return view
+    }
+
+    override fun onResume() {
+        super.onResume()
+        circularProgressBar.setProgress(50.0F,"800")
     }
 }
